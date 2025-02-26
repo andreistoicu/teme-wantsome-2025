bec1039849997aa9d5dd692ee56a9e77bf11b358
andrei <andrei_stoicu@yahoo.com>
2025-02-26 16:41:13 +0200

    remove the config

--- src/main/java/org/wantsome/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package org.wantsome;
-
-import org.springframework.context.annotation.*;
-
-@Configuration
-@ComponentScan(basePackages = "org.wantsome")
-public class AppConfig {
-
-}
--- src/main/java/org/wantsome/Main.java
+++ src/main/java/org/wantsome/Main.java
@@ -5,13 +5,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
-        // Initialize the Spring context
-        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class))
-        {
-            // The context is now initialized and beans are created
-            System.out.println("Spring context initialized");
-        }
     }
 
 }
