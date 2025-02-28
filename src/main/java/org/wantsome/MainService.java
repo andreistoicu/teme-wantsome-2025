69fa1270da00bb7df5a1d0375e0c8656e6402e40
valentinchiriac <toader.chiriac@gmail.com>
2025-02-28 12:33:22 +0200

    Spring Teme Exercitiul 1

--- src/main/java/org/wantsome/MainService.java
+++ src/main/java/org/wantsome/MainService.java
@@ -2,6 +2,8 @@ package org.wantsome;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.wantsome.Week15_TemeSpring.GreetingService;
+import org.wantsome.Week15_TemeSpring.HelloWorldService;
 
 import javax.annotation.PostConstruct;
 
