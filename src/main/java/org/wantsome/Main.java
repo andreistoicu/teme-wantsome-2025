3ba2668b6c5c79b3cebfa7bef4240c8b6228b0b6
valentinchiriac <toader.chiriac@gmail.com>
2025-03-21 13:40:25 +0200

    second commit Valentin tema 2

--- src/main/java/org/wantsome/Main.java
+++ src/main/java/org/wantsome/Main.java
@@ -1,5 +1,9 @@
 package org.wantsome;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
