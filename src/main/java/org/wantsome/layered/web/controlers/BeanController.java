247dba6b4a8de7955cf55e7aaffe74309d27326a
valentinchiriac <toader.chiriac@gmail.com>
2025-03-21 12:51:17 +0200

    bean controller push inainte de tema 2 html

--- src/main/java/org/wantsome/layered/web/controlers/BeanController.java
+++ src/main/java/org/wantsome/layered/web/controlers/BeanController.java
@@ -1,13 +1,15 @@
 package org.wantsome.layered.web.controlers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BeanController {
-    @RequestMapping(method = RequestMethod.GET, values = "/users")
-    public String getUsers(){
+    @GetMapping("/users")
+    public String getUsers() {
         return "users";
     }
-}
+    }
+
