8181b4ca5443ef315bda5b7eef67c2001994a236
valentinchiriac <toader.chiriac@gmail.com>
2025-03-21 13:23:33 +0200

    test commit Valentin tema 2

--- src/main/java/org/wantsome/Main.java
+++ src/main/java/org/wantsome/Main.java
@@ -2,9 +2,10 @@ package org.wantsome;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello All");
+        SpringApplication.run(Main.class, args);
     }
-
 }
